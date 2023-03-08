68ddd55643e2953a4958625aeae73a4975ad8dd0
shanghaik168@gmail.com <Shanghaik1998>
2023-03-08 11:21:27 +0700

    Add project files.

---
+++ Shopping_Application/Configurations/ProductConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace Shopping_Application.Configurations
+{
+    public class ProductConfiguration
+    {
+
+    }
+}
---
+++ Shopping_Application/Models/BillDetail.cs
@@ -0,0 +1,12 @@
+﻿namespace Shopping_Application.Models
+{
+    public class BillDetail
+    {
+        public Guid ID { get; set; }
+        public Guid IDHD { get; set; }
+        public Guid IDSP { get; set; }
+        public int Quantity { get; set; }
+        public int Price { get; set;}
+        public virtual Bill Bill { get; set; }
+    }
+}
